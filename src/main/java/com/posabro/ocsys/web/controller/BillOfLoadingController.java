123664b3183d58ba6da1f07bdb8663270b23b6fa
Carlos <Carlos@Carlos-PC>
2013-05-17 09:11:07 -0400

    findOutboundBol method added

--- src/main/java/com/posabro/ocsys/web/controller/BillOfLoadingController.java
+++ src/main/java/com/posabro/ocsys/web/controller/BillOfLoadingController.java
@@ -10,6 +10,7 @@ import com.posabro.ocsys.domain.OutboundBolPK;
 import com.posabro.ocsys.services.OutboundBolService;
 import com.posabro.ocsys.web.UserInfoProvider;
 import com.posabro.web.controller.ValidationController;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +18,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -33,9 +35,16 @@ public class BillOfLoadingController extends ValidationController{
     private OutboundBolService outboundBolService;
     
     @RequestMapping(value="storeOutboundBol", method = RequestMethod.POST)
-    public void store(@Valid @RequestBody OutboundBol outboundBol){
+    public void store(@Valid @RequestBody OutboundBol outboundBol, HttpServletResponse response){
         BranchPK branchPK = UserInfoProvider.getLoggedUser().getBranch().getBranchPK();
+        logger.debug("store a new outboundBol for branch : " + branchPK);
         outboundBolService.saveOutboundBol(branchPK, outboundBol);
+    }
+    
+    @RequestMapping(value="findOutboundBol", method = RequestMethod.POST)
+    public @ResponseBody OutboundBol findOutboundBol(@Valid @RequestBody OutboundBolPK outboundBolPK){
+        OutboundBol foundOutboundBol = outboundBolService.findOutboundBol(outboundBolPK);
+        return foundOutboundBol;
     }
     
     @RequestMapping(value="outboundBolToPdf", method = RequestMethod.GET)
