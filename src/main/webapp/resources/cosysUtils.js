a1865ec649c680a79a9a5536a6105066cb11452e
jaime hesiquio herrera <jaime.hesiquio@gmail.com>
2013-05-20 12:10:46 -0500

    Modificados para agregar opcion de menu

--- src/main/webapp/resources/cosysUtils.js
+++ src/main/webapp/resources/cosysUtils.js
@@ -41,7 +41,11 @@ $(function() {
         var msgError;
         if(!(error instanceof jQuery)){
             //                    alert('error in field : ' + error.field + ' from object : ' + error.objectName + ' message : ' + error.defaultMessage);
-            element = $('#' + error.objectName + 'Form [id^=' + error.field + ']').addClass('ui-state-error');
+            var errorTemp = error.field.replace('.', '_');
+            //alert(errorTemp);
+            //element = $('#' + error.objectName + 'Form [id^=' + errorTemp + ']').addClass('ui-state-error');
+            //[id^=errorTemp]
+            element = $('[id^=' + errorTemp + ']').addClass('ui-state-error');
             msgError = error.defaultMessage;
         }else{
             element = error;
